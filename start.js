382305b801ed83993a7775fc56b2ac2650c0cc60
Sam Salisbury <samsalisbury@gmail.com>
2013-08-30 12:06:42 +0100

    Removed unused code

--- start.js
+++ start.js
@@ -9,22 +9,6 @@ crawl.go(domain, function (results) {
 	save_results_as_json(results, domain);
 });
 
-
-function print_results(results) {
-	console.log("Total of " + results.pages_scanned.length + " pages scanned:");
-	for(var u in results.pages_scanned) {
-		var item = results.pages_scanned[u];
-		console.log(item.url);
-		if(output_type != 'site_map_only') {
-			for(var s in item.static_resources) {
-				console.log('\t\t' + item.static_resources[s]);
-			}
-		}
-	}
-	console.log("Total of " + results.pages_scanned.length +
-		" pages scanned in " + results.time_taken + "s");
-}
-
 function save_results_as_json(results, filename) {
 	filename = filename + ".json";
 	var fs = require('fs');
