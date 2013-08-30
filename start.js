01191244f38e430a0a9fe4c2e73e89db5ee269f1
Sam Salisbury <samsalisbury@gmail.com>
2013-08-30 11:59:59 +0100

    Output results to JSON

--- start.js
+++ start.js
@@ -5,7 +5,10 @@ var crawl = require('./crawl.js');
 var domain = process.argv.slice(2)[0];
 var output_type = process.argv.slice(2)[1];
 
-crawl.go(domain, print_results);
+crawl.go(domain, function (results) {
+	save_results_as_json(results, domain);
+});
+
 
 function print_results(results) {
 	console.log("Total of " + results.pages_scanned.length + " pages scanned:");
@@ -20,4 +23,16 @@ function print_results(results) {
 	}
 	console.log("Total of " + results.pages_scanned.length +
 		" pages scanned in " + results.time_taken + "s");
+}
+
+function save_results_as_json(results, filename) {
+	filename = filename + ".json";
+	var fs = require('fs');
+	fs.writeFile(filename, JSON.stringify(results), function(err) {
+		if(err) {
+			console.log(err);
+		} else {
+			console.log("Results saved as " + filename);
+		}
+	});	
 }
