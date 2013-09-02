853e34d25be9a9db1cd5bee4748038edfcfcb9e7
Sam Salisbury <samsalisbury@gmail.com>
2013-09-02 21:09:19 +0100

    Extract logging

--- crawler.js
+++ crawler.js
@@ -1,11 +1,11 @@
 
-exports.go = function (domain, done_callback) {
-	var crawler = new Crawler(domain, done_callback);
+exports.go = function (domain, done_callback, log) {
+	var crawler = new Crawler(domain, done_callback, log);
 
 	crawler.start();
 };
 
-function Crawler(domain, done) {
+function Crawler(domain, done, log) {
 
 	var root_domain = domain;
 	var phantom = require('phantom');
@@ -98,37 +98,6 @@ function Crawler(domain, done) {
 				}
 			});
 		});
-	};
-
-	// 1 = errors only, 2 = warnings, 3 = info, 4 = debug, 5 = trace
-	var log_level = 2;
-	var log = {
-		log: function (message, level) {
-			if(!level) {
-				throw 'log.log requires you to set a level [1-5]';
-			}
-			if(log_level >= level) {
-				console.log('\n' + message);
-			}
-		},
-		error: function (message) {
-			log.log(message, 1);
-		},
-		warn: function (message) {
-			log.log(message, 2);
-		},
-		info: function (message) {
-			log.log(message, 3);
-		},
-		debug: function (message) {
-			log.log(message, 4);
-		},
-		trace: function (message) {
-			log.log(message, 5);
-		},
-		progress: function (symbol) {
-			process.stdout.write(symbol);
-		}
 	};
 
 	var with_page = function (callback) {
--- start.js
+++ start.js
@@ -1,4 +1,5 @@
 var crawler = require('./crawler.js');
+var log = require('./log.js');
 
 // Pass in a second argument site_map_only to hide messy static resource output.
 
@@ -7,7 +8,7 @@ var output_type = process.argv.slice(2)[1];
 
 crawler.go(domain, function (results) {
 	save_results_as_json(results, domain);
-});
+}, log);
 
 function save_results_as_json(results, filename) {
 	filename = filename + ".json";
